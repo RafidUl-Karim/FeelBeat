0fbf90813244842d7627e35119082146f3561f73
Rafid Ul Karim <96878852+RafidUl-Karim@users.noreply.github.com>
2024-11-29 00:45:59 +0600

    Add files via upload

--- FB_Web/FeelBeat/FeelBeat/urls.py
+++ FB_Web/FeelBeat/FeelBeat/urls.py
@@ -22,7 +22,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('User.urls')),
-    path('fer/', include('fer.urls'))
+    path('fer/', include('fer.urls')),
+
 ]
 
 
